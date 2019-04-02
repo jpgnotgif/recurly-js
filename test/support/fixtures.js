e32c035e3d225f63b357418a98703185398ab174
Christopher Rogers <chrissrogers@gmail.com>
2019-04-01 19:21:35 -0700

    Adds Element tests

--- test/support/fixtures.js
+++ test/support/fixtures.js
@@ -1,5 +1,14 @@
 import {testBed} from './helpers';
 
+const elements = opts => `
+  <form action="#" id="test-form">
+    <div id="recurly-elements"></div>
+    <input type="text" data-recurly="first_name" value="${opts.first_name || ''}">
+    <input type="text" data-recurly="last_name" value="${opts.last_name || ''}">
+    <input type="hidden" data-recurly="token" name="recurly-token">
+  </form>
+`;
+
 const minimal = opts => `
   <form action="#" id="test-form">
     <div data-recurly="number"></div>
@@ -169,6 +178,7 @@ const iframe = opts => `
 `;
 
 const FIXTURES = {
+  elements,
   minimal,
   all,
   bank,
